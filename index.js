badad2c931ebf54552c701c53d330cbb4f014c89
Justin Rodriguez <justindrod@gmail.com>
2023-10-29 10:20:07 -0500

    Adding addon

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import express from 'express'
 const app = express();
 const PORT = process.env.PORT || 5001
+const { Pool } = require('pg')
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
+})
+.get('/db/', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    res.json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
