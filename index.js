c589b6322de95fbc23b6df10c638680ddb096010
Justin Rodriguez <justindrod@gmail.com>
2023-10-31 17:31:39 -0500

    Redis upgrade

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
-import express from 'express'
+import express from 'express';
 const app = express();
-const PORT = process.env.PORT || 5001
-import pg from "pg";
+const PORT = process.env.PORT || 5001;
+import { createClient } from 'redis';
 
-const { Pool} = pg;
+const client = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false
+  }
 
+});
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-})
 app.get("/", (req, res) => {
   res.json({ message: "ok" });
 })
 .get('/db/', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    res.json(result.rows);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+  const value = await client.get("key");
+  res.json(value);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
